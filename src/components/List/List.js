9379eec22644616051aa186183ccc3504761be05
Kamil Trzcinski <asasn233@gmail.com>
2024-01-29 16:38:50 +0100

    props children in practice

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,18 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
+
 const List = () => {
     return (
       <div>
-          <header className={styles.header}>
-              <h2 className={styles.title}>Things to do<span>soon!</span></h2>
-          </header>
-          <p className={styles.description}>Interesting things I want to check out</p>
-          <section className={styles.columns}>
-              <article>
-                  <h2>Books</h2>
-              </article>
-              <article>
-                  <h2>Movies</h2>
-              </article>
-              <article>
-                  <h2>Games</h2>
-              </article>
-          </section>
+            <header className={styles.header}>
+                <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+            </header>
+            <p className={styles.description}>Interesting things I want to check out</p>
+            <section className={styles.columns}>
+                <Column title = "Books" icon= "book"/>
+                <Column title = "Movies" icon = "film"/>
+                <Column title = "Games" icon = "gamepad"/>
+            </section>
       </div>
     );
   };
